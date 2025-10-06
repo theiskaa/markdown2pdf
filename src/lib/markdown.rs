208fcc0739efbda3fc1b8a0dcdc4affcebe03e24
Ismael Sh <me@theiskaa.com>
2025-10-07 00:03:07 +0400

    feat(markdown): improve list parsing and add is_list_marker method for better list item handling

--- src/lib/markdown.rs
+++ src/lib/markdown.rs
@@ -117,6 +117,8 @@ impl Lexer {
             }
         }
 
+        let _ = Token::save_to_json_file(tokens.clone(), "tokens.json");
+
         Ok(tokens)
     }
 
@@ -147,6 +149,12 @@ impl Lexer {
                     match self.current_char() {
                         '-' | '+' => {
                             if !self.check_horizontal_rule()? {
+                                content.push(self.parse_list_item(false, current_indent)?);
+                                continue;
+                            }
+                        }
+                        '*' => {
+                            if self.is_list_marker('*') {
                                 content.push(self.parse_list_item(false, current_indent)?);
                                 continue;
                             }
@@ -188,6 +196,7 @@ impl Lexer {
 
         let token = match current_char {
             '#' if is_line_start => self.parse_heading()?,
+            '*' if is_line_start && self.is_list_marker('*') => self.parse_list_item(false, 0)?,
             '*' | '_' => self.parse_emphasis()?,
             '`' => self.parse_code()?,
             '>' if is_line_start => self.parse_blockquote()?,
@@ -601,6 +610,13 @@ impl Lexer {
                         content.push(self.parse_list_item(false, current_indent)?);
                     }
                 }
+                '*' => {
+                    if self.is_list_marker('*') {
+                        content.push(self.parse_list_item(false, current_indent)?);
+                    } else {
+                        break;
+                    }
+                }
                 '0'..='9' => {
                     if self.check_ordered_list_marker().is_some() {
                         content.push(self.parse_list_item(true, current_indent)?);
@@ -631,6 +647,21 @@ impl Lexer {
             pos += 1;
         }
         count
+    }
+
+    /// Checks if the given character at the current position is a list marker
+    /// A list marker is followed by whitespace (space or tab)
+    fn is_list_marker(&self, marker: char) -> bool {
+        if self.current_char() != marker {
+            return false;
+        }
+
+        if self.position + 1 < self.input.len() {
+            let next_char = self.input[self.position + 1];
+            next_char == ' ' || next_char == '\t'
+        } else {
+            false
+        }
     }
 }
 
